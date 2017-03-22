0676fb72dc57ae36e77bff697c0ebb766d7c3a91
Paulina <95p.kowalczyk@gmail.com>
2017-03-22 10:10:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 var element = document.getElementById('content');
 
-function dropdownStyles(select){
-    
-    var style = select.id;
-    var value = select.value;
-    
-    element.style[style] = value;
-    
+function dropdownStyles(select) {
+
+	var style = select.id;
+	var value = select.value;
+
+	element.style[style] = value;
+
 }
 
-function name(event){
-    
-    var target = event.target;
-    if(target.tagName = "select"){
-        dropdownStyles(target);
-    }
-    
-    event.stopPropagation();
+function name(event) {
+
+	var target = event.target;
+	if (target.tagName = "select") {
+		dropdownStyles(target);
+	}
 }
 
-document.getElementById("wrapper").addEventListener( 'change', name, true );
-    
+document.getElementById("wrapper").addEventListener('change', name);
